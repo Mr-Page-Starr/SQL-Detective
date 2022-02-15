0a1e5aeb731c5debfb1679540f0e042e93ea187c
HaroldStarr <99093992+MrPageStarr@users.noreply.github.com>
2022-02-15 10:47:21 +0000

    Initial commit

---
+++ main.sql
@@ -0,0 +1,9 @@
+.open sql-murder-mystery.db
+.mode column
+
+-- Write one query at a time starting on line 8 below. We have started the first one for you.
+
+-- You can then delete it once you have taken a note of your answer using your detective notes and then start your next query. 
+
+
+select * from crime_scene_report where type="murder"
